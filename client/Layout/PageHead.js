f30c855c706a9cc9c48bfaf4e57d6212f12f97a4
Shane Padgett <spadgett00@gmail.com>
2018-07-08 18:35:10 -0500

    Remove unecessary code

--- client/Layout/PageHead.js
+++ client/Layout/PageHead.js
@@ -7,13 +7,9 @@ const PageHead = ({title, description}) => (
     <meta charSet='utf-8' />
     <meta httpEquiv='content-language' content='en' />
     <meta name='viewport' content='initial-scale=1.0, width=device-width' />
-    {/* <link href='https://stackpath.bootstrapcdn.com/bootswatch/4.1.1/lux/bootstrap.min.css' rel='stylesheet' /> */}
     <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' />
-    {/* <link rel='stylesheet' href='/static/app.css' /> */}
     {/* <link href='static/css/style.css' rel='stylesheet' /> */}
-    {/* <link rel='stylesheet' href='https://use.fontawesome.com/releases/v5.1.0/css/all.css' integrity='sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt' crossorigin='anonymous' /> */}
     {/* <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css' /> */}
-
   </Head>
 )
 
