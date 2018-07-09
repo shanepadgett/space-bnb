c72212e41a5d4b2375cffd5b594137681bd512b9
josephemswiler <josephemswiler@gmail.com>
2018-07-09 08:34:26 -0500

    merge dev

--- client/lib/threeJS/sceneSubjects/menu.js
+++ client/lib/threeJS/sceneSubjects/menu.js
@@ -19,15 +19,16 @@ export default class Menu extends Component {
               <CardBody>
                 <CardTitle>{this.props.selection}</CardTitle>
                 <CardSubtitle>Information</CardSubtitle>
-                <CardText> <br />
-                Distance from Sun: 67.24 million mi <br />
-                Radius: 3,760 mi <br />
-                Orbital period: 225 days <br />
-                Mass: 4.867 × 10^24 kg (0.815 M⊕) <br />
-                Equatorial rotation velocity: 6.52 km/h (1.81 m/s) <br />
-                Average orbital speed‎: ‎35.02 km/s <br />
-                Surface gravity‎: ‎8.87 m/s2; 0.904 g <br />
-                Surface area‎: ‎4.6023×108 km2 <br />
+                <CardText>
+                  {' '}<br />
+                  Distance from Sun: 67.24 million mi <br />
+                  Radius: 3,760 mi <br />
+                  Orbital period: 225 days <br />
+                  Mass: 4.867 × 10^24 kg (0.815 M⊕) <br />
+                  Equatorial rotation velocity: 6.52 km/h (1.81 m/s) <br />
+                  Average orbital speed‎: ‎35.02 km/s <br />
+                  Surface gravity‎: ‎8.87 m/s2; 0.904 g <br />
+                  Surface area‎: ‎4.6023×108 km2 <br />
                 </CardText>
                 <Link href='/about'>
                   <Button className='btn btn-outline-danger'>
@@ -40,19 +41,25 @@ export default class Menu extends Component {
             </Card>
           </div>
         </div>
-        <style global jsx>{`        
+        <style global jsx>{`
           .card-wrapper {
             position: absolute; 
             width: 100%;
             right: 0;
             top: 100px;
-            
+          }
+
+          .card-wrapper-hide {
+            position: absolute; 
+            width: 100%;
+            right: 0;
+            top: 100px;
           }
           
           .card {
             background-color: rgba(0, 0, 0, 0.5) !important;
           }
-          
+
           .card-subtitle {
             color: #fff;
           }
