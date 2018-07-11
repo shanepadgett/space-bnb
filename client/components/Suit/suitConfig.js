95d9deb3bb7d4ebaa002417014c68dc5a77d010f
josephemswiler <josephemswiler@gmail.com>
2018-07-11 18:53:44 -0500

    updates

--- client/components/Suit/suitConfig.js
+++ client/components/Suit/suitConfig.js
@@ -1,11 +1,16 @@
-import React, { Component } from 'react';
-
-export default class suitConfig extends Component {
-  render() {
-    return (
-      <div className="suitconfig">
-        { this.props.children }
-      </div>
-    )
+export default {
+  atlas: {
+    name: 'Extravehicular Mobility Unit',
+    img: '../../static/images/atlas-rocket.png',
+    model: '9ca3dc74a66847da981a5353f3e71a68',
+    price: '24635',
+    rating: ['fas', 'fas', 'fas', 'fas', 'far'],
+    quickFacts: {
+      weight: '55.3kg',
+      operating_Pressure: '29.6kPa',
+      primary_Support: '480min',
+      backup_Support: '30min'
+    },
+    description: 'The Atlas V was developed by Lockheed Martin Commercial Launch Services as part of the US Air Force Evolved Expendable Launch Vehicle (EELV) program and made its inaugural flight on August 21, 2002. The vehicle operates out of Space Launch Complex 41 at Cape Canaveral Air Force Station and Space Launch Complex 3-E at Vandenberg Air Force Base.'
   }
 }
